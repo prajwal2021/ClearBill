eb2a64e0e3cf9f2c8f6e8b84e3c6c62ad81acfde
prajwal2021 <prajwals2021@gmail.com>
2026-01-09 13:49:33 -0600

    type error fix

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -10,13 +10,13 @@ export function createClient() {
     {
       cookies: {
         get(name: string) {
-          return cookieStore.get(name)?.value
+          return (await cookieStore).get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          cookieStore.set({ name, value, ...options })
+          (await cookieStore).set({ name, value, ...options })
         },
         remove(name: string, options: CookieOptions) {
-          cookieStore.set({ name, value: '', ...options })
+          (await cookieStore).set({ name, value: '', ...options })
         },
       },
     }
